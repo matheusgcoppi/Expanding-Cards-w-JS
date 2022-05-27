d7b898dd1509e574601b8d40708953ca1ced45fe
matheusgcoppi <matheuscoppi22@gmail.com>
2022-05-26 23:01:33 -0300

    Final

--- Project/script.js
+++ Project/script.js
@@ -1,3 +1,21 @@
 const panels = document.querySelectorAll('.panel') //All because We have more than 1 panel to select
 
-panels.forEach{}
+/*panels.forEach((panel) => {   FUNCTION - SHORT WAY
+    panel.addEventListener('click', () => {
+        console.log(123)
+    })
+}*/
+
+panels.forEach(function (panel) {  // FUNCTION - LONG WAY
+    panel.addEventListener('click', function() {
+        removeActiveClasses()
+        panel.classList.add('active')
+    }); 
+    })
+
+function removeActiveClasses () {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+
+    })
+}
